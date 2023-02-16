4bb1985546dbe14878f8857f1c2ab68e1dee4662
kavehjamshidi <kaveh.jamshidi98@gmail.com>
2023-02-16 20:06:29 +0330

    setup redis

--- bootstrap/env.go
+++ bootstrap/env.go
@@ -1,21 +1,20 @@
 package bootstrap
 
 import (
-	"log"
 	"os"
 	"time"
 )
 
 const (
 	serverAddressEnvKey              = "SERVER_ADDRESS"
-	redisURIEnvKey                   = "REDIS_URI"
+	redisAddressEnvKey               = "REDIS_ADDRESS"
 	accessTokenExpirationTimeEnvKey  = "ACCESS_EXPIRATION"
 	refreshTokenExpirationTimeEnvKey = "REFRESH_EXPIRATION"
 	accessTokenSecretEnvKey          = "ACCESS_SECRET"
 	refreshTokenSecretEnvKey         = "REFRESH_SECRET"
 
 	defaultServerAddress              = ":8080"
-	defaultRedisURI                   = "redis://6379"
+	defaultRedisAddress               = "redis://6379"
 	defaultAccessTokenExpirationTime  = "15m"
 	defaultRefreshTokenExpirationTime = "168h"
 	defaultAccessTokenSecret          = "access token secret"
@@ -24,7 +23,7 @@ const (
 
 type Env struct {
 	ServerAddress              string
-	RedisURI                   string
+	RedisAddress               string
 	AccessTokenExpirationTime  time.Duration
 	RefreshTokenExpirationTime time.Duration
 	AccessTokenSecret          string
@@ -33,24 +32,24 @@ type Env struct {
 
 func NewEnv() *Env {
 	serverAddress := getEnvWithFallback(serverAddressEnvKey, defaultServerAddress)
-	redisURI := getEnvWithFallback(redisURIEnvKey, defaultRedisURI)
+	redisAddress := getEnvWithFallback(redisAddressEnvKey, defaultRedisAddress)
 	accessTokenSecret := getEnvWithFallback(accessTokenSecretEnvKey, defaultAccessTokenSecret)
 	refreshTokenSecret := getEnvWithFallback(refreshTokenSecretEnvKey, defaultRefreshTokenSecret)
 
 	accessTokenExpirationTimeString := getEnvWithFallback(accessTokenExpirationTimeEnvKey, defaultAccessTokenExpirationTime)
 	accessTokenExpirationTime, err := time.ParseDuration(accessTokenExpirationTimeString)
 	if err != nil {
-		log.Fatalf("could not parse access token expiration time: %v\n", err)
+		panic(err)
 	}
 	refreshTokenExpirationTimeString := getEnvWithFallback(refreshTokenExpirationTimeEnvKey, defaultRefreshTokenExpirationTime)
 	refreshTokenExpirationTime, err := time.ParseDuration(refreshTokenExpirationTimeString)
 	if err != nil {
-		log.Fatalf("could not parse refresh token expiration time: %v\n", err)
+		panic(err)
 	}
 
 	return &Env{
 		ServerAddress:              serverAddress,
-		RedisURI:                   redisURI,
+		RedisAddress:               redisAddress,
 		AccessTokenExpirationTime:  accessTokenExpirationTime,
 		RefreshTokenExpirationTime: refreshTokenExpirationTime,
 		AccessTokenSecret:          accessTokenSecret,
---
+++ redis/redis.go
@@ -0,0 +1,19 @@
+package redis
+
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient(ctx context.Context, addr string) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: addr,
+	})
+
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		panic(err)
+	}
+
+	return rdb
+}
